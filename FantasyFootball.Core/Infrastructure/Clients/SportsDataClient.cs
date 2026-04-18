4dd67c55d50eca03771b91b80ec510e53b675198
Chris Pichler <cpichle1@emich.edu>
2026-04-17 21:04:56 -0400

    Remove Web folder and put client in Infrastructure folder

---
+++ FantasyFootball.Core/Infrastructure/Clients/SportsDataClient.cs
@@ -0,0 +1,19 @@
+﻿using FantasyFootball.Core.Infrastructure.Dto;
+using FantasyFootball.Models.Dto;
+
+namespace FantasyFootball.Core.Infrastructure.Clients;
+
+public sealed class SportsDataClient(HttpClient httpClient) : BaseHttpClient(httpClient), ISportsDataClient
+{
+    public async Task<IEnumerable<Player>> GetAllPlayers()
+    {
+        var httpResult = await ExecuteGet<IEnumerable<Player>>(HttpClientConstants.SportsDataGetAllPlayersPath).ConfigureAwait(false);
+
+        if (!httpResult.IsSuccess)
+        {
+            throw new HttpRequestException(BuildErrorMessage(HttpClientConstants.SportsDataGetAllPlayersPath, httpResult.ErrorMessage));
+        }
+
+        return httpResult.Data ?? throw new InvalidOperationException($"The {HttpClientConstants.SportsDataGetAllPlayersPath} endpoint returned a null response body.");
+    }
+}
